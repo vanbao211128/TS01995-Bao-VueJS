a0d782f95df21fe30644ab306649355b5c0787d4
TS01995-Bao-VueJS <baonvts01995@gmail.com>
2026-01-26 14:54:42 +0700

    Bai 2 Lab 5

--- Lab5/src/App.vue
+++ Lab5/src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import Bai1 from './components/Bai1.vue';
+import Bai2 from './components/Bai2.vue';
 
 // import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <Bai1/>
+  <Bai2/>
 </template>
 
 <style scoped>
---
+++ Lab5/src/components/Bai2.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+<input v-model="user" placeholder="Nhập dữ liệu vào">
+<p>Giá trị: {{ user }}</p>
+
+    </div>
+</template>
+
+<script setup>
+   import { ref } from 'vue';
+   const user = ref('FPL');
+</script>
+
+<style lang="scss" scoped>
+
+</style>
