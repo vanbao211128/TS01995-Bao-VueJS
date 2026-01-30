3eb47883400255cb9f06f06a87f1b64f7912a82a
TS01995-Bao-VueJS <baonvts01995@gmail.com>
2026-01-30 11:42:08 +0700

    Lab 6 Bai 2

---
+++ Lab6/src/components/Bai2.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container">
+        <h3>Nhập tháng của bạn: </h3>
+        <input type="number" v-model.number="month" />
+        <p v-if="month < 1 || month > 12" style="color: red;">Vui lòng nhập tháng từ 1 đến tháng 12</p>
+        <p v-else-if="month >= 1 && month <= 3">Mùa xuân</p>
+        <p v-else-if="month <= 6">Mùa hè</p>
+        <p v-else-if="month <= 9">Mùa thu</p>
+        <p v-else>Mùa đông</p>
+
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+const month = ref(1);
+</script>
+
+<style lang="scss" scoped></style>
