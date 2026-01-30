71dc736623f2520b03160d77db6e23a02eb13667
TS01995-Bao-VueJS <baonvts01995@gmail.com>
2026-01-30 11:18:49 +0700

    Lab 6

---
+++ Lab6/src/components/Bai1.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+<h3>Nhập điểm của bạn</h3>
+<input type="number" v-model="score" min="0" max="10"/>
+<p v-if="score >= 9">Xuất sắc</p>
+<p v-else-if="score >= 8">Giỏi</p>
+<p v-else-if="score >= 6.5">Khá</p>
+<p v-else-if="score >= 5">Trung bình</p>
+<p v-else>Yếu</p>
+    </div>
+</template>
+
+<script setup>
+    import { ref } from 'vue';
+    const score = ref(0);
+</script>
+
+<style lang="scss" scoped>
+
+</style>
