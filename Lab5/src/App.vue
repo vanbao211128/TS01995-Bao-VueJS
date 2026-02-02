fcb57808f6678620270f2a2b8eb4d73884a021bd
TS01995-Bao-VueJS <baonvts01995@gmail.com>
2026-02-02 21:38:47 +0700

    Update Lab 5

--- Lab5/src/App.vue
+++ Lab5/src/App.vue
@@ -1,46 +1,32 @@
 <script setup>
-import { ref } from 'vue';
+import Bai1 from './components/Bai1.vue';
+import Bai2 from './components/Bai2.vue';
+import Bai3 from './components/Bai3.vue';
 import Bai4 from './components/Bai4.vue';
-import Bai4a from './components/Bai4a.vue';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-const posts = ref([])
-
-function addpost(post) {
-  posts.value.push(post)
-}
-
-// import Bai3 from './components/Bai3.vue';
-
-// import Bai1 from './components/Bai1.vue';
-// import Bai2 from './components/Bai2.vue';
-
-// import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <!-- <Bai1/>
-  <Bai2/> -->
-  <!-- <Bai3/> -->
+ <Bai1/>
+  <Bai2/> 
+<Bai3/> 
+   <!-- <Bai4/> -->
   <div>
-
-    <Bai4 @add-post="addpost" />
-    <Bai4a :posts="posts" />
+    
   </div>
+ 
 </template>
 
-<style>
+<style scoped>
 .logo {
   height: 6em;
   padding: 1.5em;
   will-change: filter;
   transition: filter 300ms;
 }
-
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
-
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
